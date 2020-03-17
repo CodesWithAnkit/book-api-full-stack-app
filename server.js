a3c751c2e9356e9e99bcdfbdf3ccdee0dd7f72d1
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-03-17 11:54:17 +0530

    Basic setup of server and having ejs layout and local DB connection

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
 const app = express();
+const expressLayout = require('express-ejs-layouts');
+const indexRoute = require('./routes/index');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+mongoose.connect(
+  process.env.DB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  },
+  () => console.log('connected to DB')
+);
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayout);
+app.use(express.static('public'));
+
+app.use('/', indexRoute);
 
 app.listen(5000, () => console.log('Listening at PORT 5000'));
